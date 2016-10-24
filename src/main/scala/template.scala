6416ef0d4d63cf23a4f08464fb716a75bbcc01f6
skidlucas <root@sandbox.hortonworks.com>
2016-10-24 13:51:03 +0000

    Q4 Done

--- src/main/scala/template.scala
+++ src/main/scala/template.scala
@@ -40,22 +40,40 @@ object spamFilter {
   }
 
 
-  /*def computeMutualInformationFactor(
+  def computeMutualInformationFactor(
     probaWC:RDD[(String, Double)],
     probaW:RDD[(String, Double)],
     probaC: Double,
     probaDefault: Double // default value when a probability is missing
   ):RDD[(String, Double)] = {
-
-  // Code to complete...
-
-  }*/
+    //We'll take the left join of probaW (proba of the word occurs in a file) with probaWC (proba word occurs in a file of a class)
+    //Using left join because we have a sum in mutual information formula
+    //Output is like (Word, (probaOccurs, Some(probaInClass)))
+    val probaOccursAndProbaInClass = probaW.leftOuterJoin(probaWC)
+    //For each entry if the map has a proba for the class let it or put the default one if value is none
+    //Output is like (Word, (probaOccurs, probaInClass or probaDefault))
+    val probaOccursAndCleanProbaClass = probaOccursAndProbaInClass.map(x => if (x._2._2 == None) (x._1, (x._2._1, probaDefault)) else (x._1, (x._2._1, x._2._2.get)))
+    //Apply the formula
+    val res = probaOccursAndCleanProbaClass.mapValues({
+        case (x ,y) =>  y * math.log(y /(x * probaC))
+    })
+    //return value
+    res
+  }
 
   def main(args: Array[String]) {
 	   val conf = new SparkConf().setAppName("SpamFilter")
   	 val sc = new SparkContext(conf)
      //TEST probaWordDir
-     var res = probaWordDir(sc)("/tmp/tp4/*.txt")
+     //var res = probaWordDir(sc)("/tmp/tp4/*.txt")
+     //TEST
+     //TEST computeMutualInformationFactor
+     //The test doesn't work because the scala version of spark doesn't support Map()
+     //val WC = Map("Bonjour" -> 0.5, "Salut" -> 0.0)
+     //val W = Map("Bonjour" -> 0.75, "Salut" -> 0.5)
+     //val C = 0.5
+     //val Default = 1.0
+     //var res = computeMutualInformationFactor(sc.parallelize(WC.toSeq), sc.parallelize(W.toSeq), C, Default)
      //TEST
 	   println("Hello World")
   }
