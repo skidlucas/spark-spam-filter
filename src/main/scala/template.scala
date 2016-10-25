b0f5322c12be93f74c808b01fdcb4fdd3c9b81be
skidlucas <root@sandbox.hortonworks.com>
2016-10-25 08:16:00 +0000

    main done + update char array

--- src/main/scala/template.scala
+++ src/main/scala/template.scala
@@ -17,7 +17,8 @@ object spamFilter {
 	    var allTxtSplitFiles = allTxtFiles.map(e => (e._1, e._2.split("\\s+").distinct.toList))
 
 	    //Create array with non informative word (scala world)
-	    val nonInformativeWordsScala = Array(".",":",","," ","/","\\","-","","(",")","@")
+      //Add "!"
+	    val nonInformativeWordsScala = Array(".",":",","," ","/","\\","-","","(",")","@","!")
 
 	    //Remove non informative words
       //Take care of empty entry in e._2
@@ -34,7 +35,7 @@ object spamFilter {
       //probability of a word is occurence number / nb files
       val probaWord = wordDirOccurency.map(e => (e._1, (e._2.toDouble / nbFiles)))
       //DEBUG
-      probaWord.collect().foreach(println)
+      //probaWord.collect().foreach(println)
       //DEBUG
       (probaWord, nbFiles)
   }
@@ -75,7 +76,63 @@ object spamFilter {
      //val Default = 1.0
      //var res = computeMutualInformationFactor(sc.parallelize(WC.toSeq), sc.parallelize(W.toSeq), C, Default)
      //TEST
-	   println("Hello World")
+     //REAL main
+     //Compute word probabilities for the ham directory
+     val (probaWordHam, nbFilesHam) = probaWordDir(sc)("/tmp/ling-spam/ham/*.txt")
+     //Compute word probabilities for the spam directory
+     val (probaWordSpam, nbFilesSpam) = probaWordDir(sc)("/tmp/ling-spam/spam/*.txt")
+
+     //We need to have the proba of ham and spam
+     val totalFiles = (nbFilesHam + nbFilesSpam).toDouble
+     val probaSpam = nbFilesSpam / totalFiles
+     val probaHam = nbFilesHam / totalFiles
+
+     //Compute proba (occurs and ham) for word in ham = probaWordHam * probaHam
+     val occursAndHam = probaWordHam.map(e => (e._1, e._2 * probaHam))
+     //Compute proba (occurs and spam) for word in spam = probaWordSpam * probaSpam
+     val occursAndSpam = probaWordSpam.map(e => (e._1, e._2 * probaSpam))
+     //Compute proba (doesn't occur and ham) for word in ham = (1 - probaWordHam) * probaHam
+     //(1 - probaWordHam) because a file is either spam or ham
+     val noOccursAndHam = probaWordHam.map(e => (e._1, (1.0 - e._2) * probaHam))
+     //Compute proba (doesn't occur and spam) for word in spam = (1 - probaWordSpam) * probaSpam
+     //(1 - probaWordSpam) because a file is either spam or ham
+     val noOccursAndSpam = probaWordSpam.map(e => (e._1, (1.0 - e._2) * probaSpam))
+
+     //store default value
+     val default = 0.2/totalFiles
+     //To retrieve all the words we need to perform a fullouterjoin
+     //because some words are only in spam files or in ham files
+     val allWords = occursAndHam.fullOuterJoin(occursAndSpam)
+     //We need to put the default value is a value is zero
+     val allWordsDefault = allWords.mapValues { case (h, s) => (h.getOrElse(default), s.getOrElse(default))}
+     //To compute mutual information we need to have the proba of a word if it occurs whatever the class (probaW)
+     //a word is ham or spam so the result is (word, p(occursAndHam) + p(occursAndSpam))
+     val occursWords = allWordsDefault.mapValues { case (h, s) => h + s }
+     val noOccursWords = occursWords.mapValues { 1.0 - _ }
+     //Now we can calculate mutual information for every case (ham or spam and occurs or not)
+     val mutualInformationMap = List(
+       //Occurs and Ham
+       computeMutualInformationFactor(occursAndHam, occursWords, probaHam, default),
+       //Occurs and Spam
+       computeMutualInformationFactor(occursAndSpam, occursWords, probaSpam, default),
+       //doesn't occur and Ham
+       computeMutualInformationFactor(noOccursAndHam, noOccursWords, probaHam, default),
+       //doesn't occur and Spam
+       computeMutualInformationFactor(noOccursAndSpam, noOccursWords, probaSpam, default)
+     )
+     //mutualInformationMap.reduce{ (e1, e2) => e1.join(e2).map(e => (e._1, e._2._1 + e._2._2))}.take(10).foreach(println)
+     //We have a list that we need to reduce to obtain mutual information of a word
+     //Using reduceLeft looks like a loop
+     //each step, make a join to merge the word and sum the mutual information part
+     val mutualInformationReduce = mutualInformationMap.reduce {(e1, e2) => e1.join(e2).map(e => (e._1, e._2._1 + e._2._2))}
+     //Take 10 top words
+     //Use a minus to have descending sort
+     val topWords = mutualInformationReduce.sortBy(- _._2).take(10)
+     //Print topWords
+     topWords.foreach(println)
+     //save in the file
+     sc.parallelize(topWords).saveAsTextFile("/tmp/topWords.txt")
+	   println("Program ended")
   }
 
 } // end of spamFilter
